589617f991a12023c9f9561bc5c3b24079f7db78
mdelobel <matthieu.delobelle@gmail.com>
2018-02-10 18:41:07 +0100

    First test socket

---
+++ Test/Sockets/C/settings.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define PORT 12345 
+#define BACKLOG 1
+
